577ac3e9b1d444435798b36bdcfc91a99cc37bc2
daigo424 <ishii.daigo@gmail.com>
2026-04-20 18:04:44 +0900

    feat: change way to retrieve env variable

--- src/apps/admin/common.py
+++ src/apps/admin/common.py
@@ -1,12 +1,20 @@
+import os
+
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-from packages.core.config import settings
+APP_DB_HOST = os.environ.get("APP_DB_HOST")
+APP_DB_NAME = os.environ.get("APP_DB_NAME")
+APP_DB_USERNAME = os.environ.get("APP_DB_USERNAME")
+APP_DB_PASSWORD = os.environ.get("APP_DB_PASSWORD")
+APP_DB_PORT = os.environ.get("APP_DB_PORT")
+APP_DATABASE_URL = f"postgresql://{APP_DB_USERNAME}:{APP_DB_PASSWORD}@{APP_DB_HOST}:{APP_DB_PORT}/{APP_DB_NAME}"
+TIMEZONE = os.environ.get("TIMEZONE")
 
-engine = create_engine(settings.app_database_url, pool_pre_ping=True)
-
+engine = create_engine(APP_DATABASE_URL, pool_pre_ping=True)
 
 def fetch_df(query: str, params: dict | None = None) -> pd.DataFrame:
+
     with engine.connect() as conn:
         result = conn.execute(text(query), params or {})
         df = pd.DataFrame(result.fetchall(), columns=list(result.keys()))
@@ -14,5 +22,5 @@ def fetch_df(query: str, params: dict | None = None) -> pd.DataFrame:
         if pd.api.types.is_datetime64_any_dtype(df[col]):
             if df[col].dt.tz is None:
                 df[col] = df[col].dt.tz_localize("UTC")
-            df[col] = df[col].dt.tz_convert(settings.timezone)
+            df[col] = df[col].dt.tz_convert(TIMEZONE)
     return df
