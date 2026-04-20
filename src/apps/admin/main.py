fb288d28c139d17b8c81726fc386a02f02dc63f3
daigo424 <ishii.daigo@gmail.com>
2026-04-20 17:13:21 +0900

    feat(admin): change root path

--- src/apps/admin/main.py
+++ src/apps/admin/main.py
@@ -6,7 +6,7 @@ if os.environ.get("ENABLE_STREAMLIT_CLOUD") == "1":
     import sys
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    src_path = os.path.abspath(os.path.join(current_dir, "..", "..", ".."))
+    src_path = os.path.abspath(os.path.join(current_dir, "..", ".."))
     if src_path not in sys.path:
         sys.path.insert(0, src_path)
 
